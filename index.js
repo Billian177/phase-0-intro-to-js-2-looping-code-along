79f4b396bc3a9612db3caf51353a4fcdb3bf4f69
bill apiyo <billian177@gmail.com>
2022-06-11 16:38:00 +0300

    commit tree ef545cda1c9f1a0790adb57b0b0d1353f89ae2ba

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(arr, event){
+    const card=[];
+    for(let i=0; i<arr.length; i++){
+        card.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return card;
+}
+
+let st=10;
+function countDown(st){
+
+    while (st>0){
+        console.log(st);
+        st-=1;
+    }
+    console.log(st);
+}
